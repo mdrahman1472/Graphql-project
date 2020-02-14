5ef42b6c725d286535c131a96f6ae9e1f56a23cc
Md Rahman <mdrahman1472@gmail.com>
2020-02-14 16:48:52 -0700

    apollo play ground

--- server/server.js
+++ server/server.js
@@ -1,13 +1,61 @@
 const express = require('express');
-const PORT = 3500;
+const {ApolloServer} = require('appollo-server-express');
+
+// fake data to populate
+const authors = [
+   {
+      id: "1",
+      info: {
+         name: "Md Rahman",
+         age: 24,
+         gender: "M",
+      }
+   },
+   {
+      id: "2",
+      info: {
+         name: "John Doe",
+         age: 33,
+         gender: "M",
+      }
+   }
+];
+
+// graphql schemma in string form
+const tyoeDefs = `{
+   type Author {
+      id: ID!
+      info: Person
+   }
+   type Person {
+      name: String!
+      age: Int
+      gender: String
+   }
+   type Query {
+      getAuthors: [Author]
+   }
+}
+`;
+
+// the resolver
+const resolvers = {
+   Query: {
+      getAuthors: () => authors,
+   }
+}
+const PORT = 3600;
+
+// put together a schemma
+const server = new ApolloSever ({typedefs, resolvers});
 
 const app = express();
 
-// '/graphql' is the endpoint I defined
-app.use('/graphql', (req, res) => {
-   res.send('Welcome to Our Author App');
+server.applyMiddleware({
+   app,
+   path: '/graphql'
 });
 
-app.listen(PORT, ()=>{
-   console.log('Server running on port: ', PORT);
+app.listen (PORT, () => {
+   console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
 });
